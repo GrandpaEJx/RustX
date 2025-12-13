91e982717ad528ac8bed0dd14baa0a1fc1db92e2
Grandpa EJ <abirmridha2005@gmail.com>
2025-12-13 07:26:45 +0600

    Add ast/expr.rs with Expression enum

---
+++ crates/core/src/ast/expr.rs
@@ -0,0 +1,62 @@
+/// Expression nodes for RustX AST
+
+/// Expression enum representing all expression types
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expr {
+    // Literals
+    Int(i64),
+    Float(f64),
+    String(String),
+    Bool(bool),
+    Null,
+    
+    // Identifier
+    Ident(String),
+    
+    // Binary operations
+    Binary {
+        left: Box<Expr>,
+        op: super::BinaryOp,
+        right: Box<Expr>,
+    },
+    
+    // Unary operations
+    Unary {
+        op: super::UnaryOp,
+        expr: Box<Expr>,
+    },
+    
+    // Function call
+    Call {
+        callee: Box<Expr>,
+        args: Vec<Expr>,
+    },
+    
+    // Array literal
+    Array(Vec<Expr>),
+    
+    // Map literal
+    Map(Vec<(String, Expr)>),
+    
+    // Index access (array[index] or map[key])
+    Index {
+        object: Box<Expr>,
+        index: Box<Expr>,
+    },
+    
+    // If expression
+    If {
+        condition: Box<Expr>,
+        then_branch: Box<Expr>,
+        else_branch: Option<Box<Expr>>,
+    },
+    
+    // Block expression
+    Block(Vec<super::Stmt>),
+    
+    // Assignment
+    Assign {
+        name: String,
+        value: Box<Expr>,
+    },
+}
